6d163717e4e1fd4b1259200c0541f636fb428c4e
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-04 12:09:50 +0330

    Unsolve problemset 14742: blog

---
+++ problemset/14742/project_14742/blog/models.py
@@ -0,0 +1,40 @@
+from django.db import models
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=250)
+    body = models.TextField()
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title} - {self.date_created}"
+
+    def copy(self):
+        post = BlogPost.objects.get(pk=self.pk)
+        comments = post.comment_set.all()
+
+        post.pk = None
+        post.save()
+
+        for comment in comments:
+            comment.pk = None
+            comment.blog_post = post
+            comment.save()
+
+        return post.id
+
+
+class Comment(models.Model):
+    blog_post = models.ForeignKey(BlogPost, on_delete=models.CASCADE)
+    text = models.CharField(max_length=500)
+
+    def __str__(self):
+        return f"{self.text} - {self.blog_post}"
