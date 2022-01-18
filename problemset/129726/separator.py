413c10f6d4897f8b75058feb49f3d5110c4df729
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-18 23:35:17 +0330

    Solve problemset 129726: separator

---
+++ problemset/129726/separator.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Github: https://github.com/mohsenebrahimyir/quera
+
+### https://quera.ir/problemset/129726/ ###
+
+def is_even(i):
+    return True if i % 2 == 0 else False
+
+
+def separator(ls):
+    odd = []
+    even = []
+    
+    for i in ls:
+        if is_even(i):
+            even.append(i)
+        else:
+            odd.append(i)
+    
+    return (even, odd)
