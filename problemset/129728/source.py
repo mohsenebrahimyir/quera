bfab4510f31359bc099f3c1e5dc3d24dab71a029
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-01-18 23:35:46 +0330

    Solve problemset 129728: source

---
+++ problemset/129728/source.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Github: https://github.com/mohsenebrahimyir/quera
+
+### https://quera.ir/problemset/129728/ ###
+
+print(' '.join(sorted([(i if (ord(i) - 97) % 2 == 0 else i.upper()) for i in input()], reverse=True)))
