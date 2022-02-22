fdbaeea3c96a8bfadd1918655145b246b217bb73
mohsenebrahimyir <mohsenebrahimy@outlook.com>
2022-02-22 10:55:23 +0330

    add test to problemst 589

--- problemset/589/factorial.py
+++ problemset/589/factorial.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 # Github: https://github.com/mohsenebrahimyir/quera
-
 ### https://quera.ir/problemset/589/ ###
 
-n = input()
-n = int(n)
 
-factorial = 1
+def factorial():
+    n = input()
+    n = int(n)
 
-for i in range(n):
-    factorial *= n - i
+    fact = 1
+    for i in range(n):
+        fact *= n - i
+
+    return fact
 
 
-print(factorial)
+if __name__ == '__main__':
+    result = factorial()
+    print(result)
---
+++ problemset/589/test_factorial.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# Github: https://github.com/mohsenebrahimyir/quera
+### https://quera.ir/problemset/589/ ###
+
+import unittest
+from unittest.mock import patch
+
+from factorial import factorial
+
+
+class TestListMatchSearchs(unittest.TestCase):
+        
+    @patch('builtins.input', return_value=5)
+    def test_factorial_5(self, mock_inputs):
+        result = factorial()
+        self.assertEqual(result, 120)
+
+
+if __name__ == '__main__':
+    unittest.main()
